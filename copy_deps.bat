5b6ed1a6644f9d38e59f09503d5b4b828c2133a1
Imantion <daniil.kolomiets.00@gmail.com>
2026-04-08 12:20:49 +0300

    Configuring project

---
+++ copy_deps.bat
@@ -0,0 +1,41 @@
+@echo off
+:: ===========================================
+:: copy_dependencies.bat
+:: Copies SFML DLLs and data folder next to binary
+:: Does NOT overwrite existing files
+:: Usage: copy_dependencies.bat <TargetDir> <Configuration>
+:: ===========================================
+
+set TARGETDIR=%1
+set CONFIG=%2
+
+if "%TARGETDIR%"=="" (
+    echo ERROR: No target directory specified
+    exit /b 1
+)
+
+:: Determine SFML DLL suffix
+if /I "%CONFIG%"=="Debug" (
+    set SUFFIX=-d-3.dll
+) else (
+    set SUFFIX=-3.dll
+)
+
+echo Copying SFML DLLs for %CONFIG% to %TARGETDIR%...
+
+:: List of SFML modules
+set MODULES=graphics window system audio network
+
+for %%M in (%MODULES%) do (
+    if not exist "%TARGETDIR%sfml-%%M%SUFFIX%" (
+        copy /Y "%~dp0SFML-3.0.2\bin\sfml-%%M%SUFFIX%" "%TARGETDIR%"
+    )
+)
+
+:: Copy data folder
+if exist "%~dp0data" (
+    echo Copying data folder...
+    xcopy "%~dp0data" "%TARGETDIR%data" /E /I /Y /Q
+)
+
+echo Dependencies copied successfully.
